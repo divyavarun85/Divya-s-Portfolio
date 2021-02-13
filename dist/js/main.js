1ba7c25a6e807137ef3c1bd8ec6f89871f405bb1
divyavarun85 <divyavarun85@gmail.com>
2021-02-13 15:41:29 -0500

    first version

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,7 @@ const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav')
 const navItems = document.querySelectorAll('.menu-nav__item');
+const projectButtons = document.querySelectorAll('.projects__btns');
 
 
 let showMenu = false;
